34e78f6ec412489b005ba37ee492c2de79caea42
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-01-29 13:55:58 +0530

    Create hh.c

---
+++ hh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+
+    printf("Enter an alpha: ");
+    scanf("%c",&c);
+
+    // evaluates to 1 (true) if c is a lowercase vowel
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    // evaluates to 1 (true) if c is an uppercase vowel
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // evaluates to 1 (true) if either isLowercaseVowel or isUppercaseVowel is true
+    
+        printf(\n"%c is a vowel.", c);
+    else
+        printf(\n"%c is a consonant.", c);
+    return 0;
+}
