bd72fae657e286bf91c47d68429451cc94a4b9b4
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-01-29 14:22:58 +0530

    Create muk.c

---
+++ muk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+* C Program to print "Hello World" 5 times using for loop
+*/
+#include <stdio.h>
+ 
+int main(){
+    int counter;
+    for(counter = 0; counter < 5; counter++){
+        printf(\n"Hello World");
+    }
+    return 0;
+}
