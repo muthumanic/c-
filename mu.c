e3e25622d2c755205a052099973eaaf815839a06
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-01-29 13:39:52 +0530

    Create mu.c

---
+++ mu.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+include<conio.h>
+int main()
+{
+    double number;
+
+    printf("Enter a positive num: ");
+    scanf("%lf", &num);
+
+    if (number <= 0.0)
+    {
+        if (number == 0.0)
+            printf(\n"You entered 0.");
+        else
+            printf(\n"You entered a nega num:");
+    }
+    else
+        printf(\n"You entered a positive num:");
+    return 0;
