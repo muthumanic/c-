06444885063ffa1c5dea84397ae98e193e60d2e9
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-01-31 15:01:19 +0530

    Create five.c

---
+++ five.c
@@ -0,0 +1,11 @@
+#include<conio.h>
+#inlcude<conio.h>
+#include <iostream.h>
+
+using namespace std;
+
+int main() 
+{ 
+    for(double num=1; num<=5; num++)
+        cout << num << "*3=" << num*3 << endl;
+}
